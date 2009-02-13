4d63b75a9e09248ca93a1a38426af6c9f50afcfd
nkohari <nkohari@120300b3-9e3e-0410-8997-0939a4f6c7b0>
2009-02-13 02:12:39 +0000

    git-svn-id: http://ninject.googlecode.com/svn/experiments/ninject2@138 120300b3-9e3e-0410-8997-0939a4f6c7b0

--- src/Ninject/Infrastructure/Language/ExtensionsForICustomAttributeProvider.cs
+++ src/Ninject/Infrastructure/Language/ExtensionsForICustomAttributeProvider.cs
@@ -1,32 +1,33 @@
+#region License
+// Author: Nate Kohari <nkohari@gmail.com>
+// Copyright (c) 2007-2009, Enkari, Ltd.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+#region Using Directives
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
+#endregion
 
 namespace Ninject.Infrastructure.Language
 {
 	internal static class ExtensionsForICustomAttributeProvider
 	{
-		public static T GetOneAttribute<T>(this ICustomAttributeProvider member)
-			where T : Attribute
-		{
-			return member.GetAttributes<T>().SingleOrDefault();
-		}
-
-		public static object GetOneAttribute(this ICustomAttributeProvider member, Type type)
-		{
-			return member.GetAttributes(type).SingleOrDefault();
-		}
-
 		public static IEnumerable<T> GetAttributes<T>(this ICustomAttributeProvider member)
 			where T : Attribute
 		{
 			return member.GetCustomAttributes(typeof(T), true) as T[];
-		}
-
-		public static IEnumerable<object> GetAttributes(this ICustomAttributeProvider member, Type type)
-		{
-			return member.GetCustomAttributes(type, true);
 		}
 
 		public static bool HasAttribute<T>(this ICustomAttributeProvider member)
--- src/Ninject/Modules/Module.cs
+++ src/Ninject/Modules/Module.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿#region License
+// Author: Nate Kohari <nkohari@gmail.com>
+// Copyright (c) 2007-2009, Enkari, Ltd.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+#region Using Directives
+using System;
 using System.Collections.Generic;
 using Ninject.Events;
 using Ninject.Infrastructure.Language;
 using Ninject.Planning.Bindings;
 using Ninject.Syntax;
+#endregion
 
 namespace Ninject.Modules
 {
