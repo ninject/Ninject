f68149d57cb22669e82dabec4017905ca2bb6942
unknown <idavis@.tometasoft.com>
2009-08-23 15:49:15 -0700

    Updating Silverlight 2.0 NAnt configuration and adding Silverlight 3 to the build batch file.

--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,5 @@
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" package %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-2.0" package %1 %2 %3 %4 %5 %6 %7 %8
+tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" package %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0" package %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=netcf-3.5" package %1 %2 %3 %4 %5 %6 %7 %8
