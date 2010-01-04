ff859ed725eff4ea0871d51f9a87823c4ea482b4
Ian Davis <idavis@innovatian.com>
2010-01-04 07:26:42 -0800

    Fixing OnActivation and OnDeactivation bug with fix from Dave Griffiths. Adding unit tests.

---
+++ src/Ninject.Tests/Integration/ActivationStrategyTests.cs
@@ -0,0 +1,60 @@
+﻿using Ninject.Tests.Fakes;
+using Xunit;
+using Xunit.Should;
+
+namespace Ninject.Tests.Integration
+{
+	public class ActivationStrategyTests
+	{
+		[Fact]
+		public void InstanceIsActivatedOnCreation()
+		{
+			using ( var kernel = new StandardKernel() )
+			{
+				kernel.Bind<Barracks>()
+					.ToSelf()
+					.OnActivation(instance =>
+									{
+										instance.Warrior = new FootSoldier();
+										instance.Weapon = new Shuriken();
+									});
+
+				var barracks = kernel.Get<Barracks>();
+				barracks.Warrior.ShouldNotBeNull();
+				barracks.Warrior.ShouldBeInstanceOf<FootSoldier>();
+				barracks.Weapon.ShouldNotBeNull();
+				barracks.Weapon.ShouldBeInstanceOf<Shuriken>();
+			}
+		}
+
+		[Fact]
+		public void InstanceIsDeactivatedWhenItLeavesScope()
+		{
+			Barracks barracks;
+			using ( var kernel = new StandardKernel() )
+			{
+				kernel.Bind<Barracks>()
+					.ToSelf()
+					.InSingletonScope()
+					.OnActivation(instance =>
+									{
+										instance.Warrior = new FootSoldier();
+										instance.Weapon = new Shuriken();
+									})
+					.OnDeactivation(instance =>
+									{
+										instance.Warrior = null;
+										instance.Weapon = null;
+									});
+
+				barracks = kernel.Get<Barracks>();
+				barracks.Warrior.ShouldNotBeNull();
+				barracks.Warrior.ShouldBeInstanceOf<FootSoldier>();
+				barracks.Weapon.ShouldNotBeNull();
+				barracks.Weapon.ShouldBeInstanceOf<Shuriken>();
+			}
+			barracks.Warrior.ShouldBeNull();
+			barracks.Weapon.ShouldBeNull();
+		}
+	}
+}
