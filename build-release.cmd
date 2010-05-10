66cbf4a26e532547fe87dfbb2134c2e46b44d60a
Ian Davis <idavis@innovatian.com>
2010-05-09 17:03:52 -0700

    Adding SL4 support!

--- build-release.cmd
+++ build-release.cmd
@@ -4,6 +4,7 @@ tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=netcf-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
