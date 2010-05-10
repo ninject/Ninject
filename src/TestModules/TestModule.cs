5c9b07f2f7d77b5ef9107f02f72a3f6a744483d2
Ian Davis <idavis@innovatian.com>
2010-05-09 18:14:33 -0700

    Adding TestModule project to make all unit tests pass.

---
+++ src/TestModules/TestModule.cs
@@ -0,0 +1,11 @@
+﻿using Ninject.Modules;
+
+namespace Ninject.Tests.TestModules
+{
+    public class TestModule : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
