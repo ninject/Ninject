f41d7256b268cc02f7775644b90c2de28df8f247
Ian Davis <idavis@innovatian.com>
2010-05-27 06:39:59 -0700

    Adding net-4.0 and net-4.0-no-web targets.

--- build-release.cmd
+++ build-release.cmd
@@ -2,6 +2,8 @@ tools\nant\nant.exe -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build package-source %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-4.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
 tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
