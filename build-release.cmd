360e608701a1fdd30d3645624a6689164c164303
Ian Davis <idavis@innovatian.com>
2010-05-28 09:28:41 -0700

    Manually merging changes from Daniel Marbach: After successful build or failed build the SharedAssemblyInfo.cs is reverted. When one of the builds fails the whole operation is aborted and the build is indicated as failed.

--- build-release.cmd
+++ build-release.cmd
@@ -1,13 +1,47 @@
-tools\nant\nant.exe -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build package-source %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-4.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-4.0-wp7" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-tools\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=netcf-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+@echo off
+set nantfile=Ninject.build
+set nantexe=tools\nant\nant.exe
+set buildlog=Ninject-Nant-Build.log
+set unittestlog=Ninject-Nant-unit-tests.log
+
+%nantexe% -buildfile:%nantfile% clean %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% package-source %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=net-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=net-3.5-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=net-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=net-4.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=silverlight-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=silverlight-3.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=silverlight-4.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=silverlight-4.0-wp7" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=mono-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=mono-2.0-no-web" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:build.config=release" "-D:build.platform=netcf-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+
+IF ERRORLEVEL 1 GOTO Failed
+
+echo "Release build completed."
+GOTO End
+
+:Failed
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+echo "============================================================"
+echo "BUILD FAILED"
+echo "============================================================"
+
+:End
+pause
