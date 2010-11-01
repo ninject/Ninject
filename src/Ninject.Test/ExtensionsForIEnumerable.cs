bb8d62980b70f37bf77c07cd95626afecd9bf94c
Remo Gloor <remo.gloor@bbv.ch>
2010-11-02 00:56:33 +0100

    Added missing file

---
+++ src/Ninject.Test/ExtensionsForIEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ninject.Tests
+{
+    public static class ExtensionsForIEnumerable
+    {
+        public static void Map<T>(this IEnumerable<T> series, Action<T> action)
+        {
+            foreach (T item in series)
+                action(item);
+        }
+    }
+}
