4bc1465623c2fe70a9d3d8f4308c9de32c55033d
Remo Gloor <remo.gloor@bbv.ch>
2011-12-19 01:44:55 +0100

    Added batch files to create pre release nuget packages

---
+++ build-alpha.cmd
@@ -0,0 +1,32 @@
+@echo off
+set nantfile=Ninject.build
+set nantexe=tools\nant\nant.exe
+set buildlog=Ninject-Nant-Build.log
+set unittestlog=Ninject-Nant-unit-tests.log
+
+%nantexe% -buildfile:%nantfile% clean %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:product.additionalVersionTag=-alpha" package-source %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% "-D:product.additionalVersionTag=-alpha" "-D:build.config=release" allPlatforms %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+
+IF ERRORLEVEL 1 GOTO Failed
+
+echo "Release build completed."
+GOTO End
+
+:Failed
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+echo "============================================================"
+echo "BUILD FAILED"
+echo "============================================================"
+
+IF NOT "%NoPause%"=="true" goto END 
+exit /B 1
+
+:End
+if "%NoPause%"=="true" goto ENDBATCHFILE 
+pause
+:ENDBATCHFILE
