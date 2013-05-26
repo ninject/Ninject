4cf3b0f7c76aa9e49f1d40268058feae1c248b14
Remo Gloor <remo.gloor@gmail.com>
2013-05-26 03:34:52 +0200

    Added documentation for WeakReferenceEqualityComparer

--- src/Ninject/Activation/Caching/WeakReferenceEqualityComparer.cs
+++ src/Ninject/Activation/Caching/WeakReferenceEqualityComparer.cs
@@ -7,11 +7,22 @@ namespace Ninject.Activation.Caching
 
     public class WeakReferenceEqualityComparer : IEqualityComparer<object>
     {
+        /// <summary>
+        /// Returns if the specifed objects are equal.
+        /// </summary>
+        /// <param name="x">The first object.</param>
+        /// <param name="y">The second object.</param>
+        /// <returns>True if the objects are equal; otherwise false</returns>
         public new bool Equals(object x, object y)
         {
             return x.Equals(y);
         }
 
+        /// <summary>
+        /// Returns the hash code of the specified object.
+        /// </summary>
+        /// <param name="obj">The object for which the hash code is calculated.</param>
+        /// <returns>The hash code of the specified object.</returns>
         public int GetHashCode(object obj)
         {
             var weakReference = obj as ReferenceEqualWeakReference;
