bfd344cd501ee1b03b4ca4f113fdce1440593a3a
Remo Gloor <remo.gloor@gmail.com>
2013-07-19 17:03:10 +0200

    Fixed that multiple bindings are returned for generic type definitions Closes #92

---
+++ src/Ninject.Test/Integration/GenericBindings.cs
@@ -0,0 +1,28 @@
+﻿namespace Ninject.Tests.Integration
+{
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class GenericBindings
+    {
+        [Fact]
+        public void ShouldReturnASingleBindingOnly()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind(typeof(GenericService<>)).ToSelf().InTransientScope();
+
+            var bindings = kernel.GetBindings(typeof(GenericService<>)).ToList();
+
+            bindings.Distinct().Count().Should().Be(1);
+
+            bindings.Count.Should().Be(1);
+        }
+
+        public class GenericService<T>
+        {
+        }
+    }
+}
