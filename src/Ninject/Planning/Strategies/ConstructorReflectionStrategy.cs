93c8d443f53745abe6fb028c73ef587e6835d145
Remo Gloor <remo.gloor@gmail.com>
2014-04-25 12:55:04 +0200

    Evaluate InjectAttribute on Constructors during Planning Slightly improves performance

--- src/Ninject/Planning/Strategies/ConstructorReflectionStrategy.cs
+++ src/Ninject/Planning/Strategies/ConstructorReflectionStrategy.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Ninject.Components;
 using Ninject.Infrastructure;
+using Ninject.Infrastructure.Language;
 using Ninject.Injection;
 using Ninject.Planning.Directives;
 using Ninject.Selection;
@@ -64,7 +65,12 @@ namespace Ninject.Planning.Strategies
 
             foreach(ConstructorInfo constructor in constructors)
             {
-                plan.Add(new ConstructorInjectionDirective(constructor, InjectorFactory.Create(constructor)));
+                var hasInjectAttribute = constructor.HasAttribute(Settings.InjectAttribute);
+                plan.Add(
+                    new ConstructorInjectionDirective(constructor, InjectorFactory.Create(constructor))
+                    {
+                        HasInjectAttribute = hasInjectAttribute
+                    });
             }
         }
     }
