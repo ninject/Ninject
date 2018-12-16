a6a21f6a8cff71c0c78edceb2391bff23627635d
Gert Driesen <gert.driesen@telenet.be>
2018-12-16 09:21:40 +0100

    Improve performance of NinjectSettings by using simple properties instead of a Dictionary-store. (#306)
    Remove `T Get<T>(string key, T defaultValue)` and `void Set(string key, object value)` methods from (I)NinjectSettings.
    Add unit tests and benchmark.
    
    Fixes #293.

---
+++ src/Ninject.Benchmarks/NinjectSettingsBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Ninject.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class NinjectSettingsBenchmark
+    {
+        private NinjectSettings _settings;
+
+        public NinjectSettingsBenchmark()
+        {
+            _settings = new NinjectSettings();
+        }
+
+        [Benchmark]
+        public void InjectAttribute()
+        {
+            var injectAttribute = _settings.InjectAttribute;
+            if (injectAttribute == null)
+                throw new System.Exception();
+        }
+
+        [Benchmark]
+        public void CachePruningInterval()
+        {
+            var interval = _settings.CachePruningInterval;
+            if (interval == TimeSpan.Zero)
+                throw new System.Exception();
+        }
+
+        [Benchmark]
+        public void LoadExtensions()
+        {
+            var loadExtensions = _settings.LoadExtensions;
+            if (!loadExtensions)
+                throw new System.Exception();
+        }
+    }
+}
---
+++ src/Ninject.Test/Unit/NinjectSettingsTests.cs
@@ -0,0 +1,118 @@
+﻿using Ninject.Activation;
+using Ninject.Infrastructure;
+using Ninject.Tests.Fakes;
+using System;
+using Xunit;
+
+namespace Ninject.Test.Unit
+{
+    public class NinjectSettingsTests
+    {
+        private NinjectSettings _settings;
+
+        public NinjectSettingsTests()
+        {
+            _settings = new NinjectSettings();
+        }
+
+        [Fact]
+        public void InjectAttribute()
+        {
+            Assert.Equal(typeof(InjectAttribute), _settings.InjectAttribute);
+            _settings.InjectAttribute = typeof(Monk);
+            Assert.Equal(typeof(Monk), _settings.InjectAttribute);
+            _settings.InjectAttribute = null;
+            Assert.Null(_settings.InjectAttribute);
+        }
+
+        [Fact]
+        public void CachePruningInterval()
+        {
+            Assert.Equal(TimeSpan.FromSeconds(30), _settings.CachePruningInterval);
+            _settings.CachePruningInterval = TimeSpan.FromMinutes(1);
+            Assert.Equal(TimeSpan.FromMinutes(1), _settings.CachePruningInterval);
+        }
+
+        [Fact]
+        public void DefaultScopeCallback()
+        {
+            Func<IContext, object> myCallback = (context) => new object();
+
+            Assert.Same(StandardScopeCallbacks.Transient, _settings.DefaultScopeCallback);
+            _settings.DefaultScopeCallback = myCallback;
+            Assert.Same(myCallback, _settings.DefaultScopeCallback);
+            _settings.DefaultScopeCallback = null;
+            Assert.Null(_settings.DefaultScopeCallback);
+        }
+
+        [Fact]
+        public void LoadExtensions()
+        {
+            Assert.True(_settings.LoadExtensions);
+            _settings.LoadExtensions = false;
+            Assert.False(_settings.LoadExtensions);
+            _settings.LoadExtensions = true;
+            Assert.True(_settings.LoadExtensions);
+        }
+
+        [Fact]
+        public void ExtensionSearchPatterns()
+        {
+            Assert.Equal(new[] { "Ninject.Extensions.*.dll", "Ninject.Web*.dll" }, _settings.ExtensionSearchPatterns);
+            _settings.ExtensionSearchPatterns = new[] { "Ninject.*.dll" };
+            Assert.Equal(new[] { "Ninject.*.dll" }, _settings.ExtensionSearchPatterns);
+            _settings.ExtensionSearchPatterns = null;
+            Assert.Null(_settings.ExtensionSearchPatterns);
+        }
+
+        [Fact]
+        public void UseReflectionBasedInjection()
+        {
+            Assert.False(_settings.UseReflectionBasedInjection);
+            _settings.UseReflectionBasedInjection = true;
+            Assert.True(_settings.UseReflectionBasedInjection);
+            _settings.UseReflectionBasedInjection = false;
+            Assert.False(_settings.UseReflectionBasedInjection);
+        }
+
+        [Fact]
+        public void InjectNonPublic()
+        {
+            Assert.False(_settings.InjectNonPublic);
+            _settings.InjectNonPublic = true;
+            Assert.True(_settings.InjectNonPublic);
+            _settings.InjectNonPublic = false;
+            Assert.False(_settings.InjectNonPublic);
+        }
+
+        [Fact]
+        public void InjectParentPrivateProperties()
+        {
+            Assert.False(_settings.InjectParentPrivateProperties);
+            _settings.InjectParentPrivateProperties = true;
+            Assert.True(_settings.InjectParentPrivateProperties);
+            _settings.InjectParentPrivateProperties = false;
+            Assert.False(_settings.InjectParentPrivateProperties);
+        }
+
+        [Fact]
+        public void ActivationCacheDisabled()
+        {
+            Assert.False(_settings.ActivationCacheDisabled);
+            _settings.ActivationCacheDisabled = true;
+            Assert.True(_settings.ActivationCacheDisabled);
+            _settings.ActivationCacheDisabled = false;
+            Assert.False(_settings.ActivationCacheDisabled);
+        }
+
+        [Fact]
+        public void AllowNullInjection()
+        {
+            Assert.False(_settings.AllowNullInjection);
+            _settings.AllowNullInjection = true;
+            Assert.True(_settings.AllowNullInjection);
+            _settings.AllowNullInjection = false;
+            Assert.False(_settings.AllowNullInjection);
+        }
+    }
+}
