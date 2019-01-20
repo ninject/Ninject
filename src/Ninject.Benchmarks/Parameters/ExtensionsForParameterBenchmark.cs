8ee77c628fe85176c675ac42f3a57e534ee9eede
Gert Driesen <gert.driesen@telenet.be>
2019-01-20 21:15:51 +0100

    Add GetShouldInheritParameters extension method.

---
+++ src/Ninject.Benchmarks/Parameters/ExtensionsForParameterBenchmark.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using Ninject.Parameters;
+using System.Collections.Generic;
+
+namespace Ninject.Benchmarks.Parameters
+{
+    [MemoryDiagnoser]
+    public class ExtensionsForParameterBenchmark
+    {
+        private IReadOnlyList<IParameter> _parametersEmpty;
+        private IReadOnlyList<IParameter> _allParametersHaveShouldInheritSetToFalse;
+        private IReadOnlyList<IParameter> _allParametersHaveShouldInheritSetToTrue;
+        private IReadOnlyList<IParameter> _mixOfParametersWithShouldInheritSetToTrueAndFalse;
+
+        public ExtensionsForParameterBenchmark()
+        {
+            _parametersEmpty = new List<IParameter>();
+            _allParametersHaveShouldInheritSetToFalse = new List<IParameter>
+                {
+                    new ConstructorArgument("foo1", 1, false),
+                    new ConstructorArgument("foo2", 2, false),
+                    new ConstructorArgument("foo3", 3, false),
+                    new ConstructorArgument("foo4", 4, false),
+                    new ConstructorArgument("foo5", 5, false)
+                };
+            _allParametersHaveShouldInheritSetToTrue = new List<IParameter>
+                {
+                    new ConstructorArgument("foo1", 1, true),
+                    new ConstructorArgument("foo2", 2, true),
+                    new ConstructorArgument("foo3", 3, true),
+                    new ConstructorArgument("foo4", 4, true),
+                    new ConstructorArgument("foo5", 5, true)
+                };
+            _mixOfParametersWithShouldInheritSetToTrueAndFalse = new List<IParameter>
+                {
+                    new ConstructorArgument("foo1", 1, true),
+                    new ConstructorArgument("foo2", 2, true),
+                    new ConstructorArgument("foo3", 3, true),
+                    new ConstructorArgument("foo4", 4, true),
+                    new ConstructorArgument("foo5", 5, true)
+                };
+        }
+
+        [Benchmark]
+        public void GetShouldInheritParameters_ParametersIsEmpty()
+        {
+            _parametersEmpty.GetShouldInheritParameters();
+        }
+
+        [Benchmark]
+        public void GetShouldInheritParameters_AllParametersHaveShouldInheritSetToFalse()
+        {
+            _allParametersHaveShouldInheritSetToFalse.GetShouldInheritParameters();
+        }
+
+        [Benchmark]
+        public void GetShouldInheritParameters_AllParametersHaveShouldInheritSetToTrue()
+        {
+            _allParametersHaveShouldInheritSetToTrue.GetShouldInheritParameters();
+        }
+
+        [Benchmark]
+        public void GetShouldInheritParameters_MixOfParametersWithShouldInheritSetToTrueAndFalse()
+        {
+            _mixOfParametersWithShouldInheritSetToTrueAndFalse.GetShouldInheritParameters();
+        }
+    }
+}
---
+++ src/Ninject/Parameters/ExtensionsForParameter.cs
@@ -0,0 +1,63 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="ExtensionsForParameter.cs" company="Ninject Project Contributors">
+//   Copyright (c) 2007-2010 Enkari, Ltd. All rights reserved.
+//   Copyright (c) 2010-2017 Ninject Project Contributors. All rights reserved.
+//
+//   Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+//   You may not use this file except in compliance with one of the Licenses.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   or
+//       http://www.microsoft.com/opensource/licenses.mspx
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace Ninject.Parameters
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Extension methods for <see cref="IParameter"/>.
+    /// </summary>
+    internal static class ExtensionsForParameter
+    {
+        /// <summary>
+        /// Returns only parameters that should be inherited from the specified list of <see cref="IParameter"/>.
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>
+        /// The list of <see cref="IParameter"/> that should be inherited.
+        /// </returns>
+        /// <remarks>
+        /// Since this is an extension for internal use only, we do not perform an argument (null) check.
+        /// This was done for performance reasons.
+        /// </remarks>
+        internal static IReadOnlyList<IParameter> GetShouldInheritParameters(this IReadOnlyList<IParameter> parameters)
+        {
+            if (parameters.Count == 0)
+            {
+                return Array.Empty<IParameter>();
+            }
+
+            List<IParameter> inherit = new List<IParameter>();
+
+            foreach (var param in parameters)
+            {
+                if (param.ShouldInherit)
+                {
+                    inherit.Add(param);
+                }
+            }
+
+            return inherit;
+        }
+    }
+}
